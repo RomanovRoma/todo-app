1f0664b190e57f8c190a33e73416844024fd1ef0
RomanovRoma <romanian.reget@gmail.com>
2020-12-09 23:00:43 +0300

    feat: add react components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello World')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn English</li>
+    </ul>
+  )
+}
+
+const AppHeader = () => {
+  return (
+    <h1>Todo List</h1>
+  )
+}
+
+const SearchPanel = () => {
+  return <input placeholder="search" />
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root'))
